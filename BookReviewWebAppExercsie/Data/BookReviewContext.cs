4f7fc8107ff09b713f71cd8faa9b226f859a1d2c
julianilssonr <79918907+julianilssonr@users.noreply.github.com>
2021-06-10 14:02:28 +0200

    Added example data via migrations

--- BookReviewWebAppExercsie/Data/BookReviewContext.cs
+++ BookReviewWebAppExercsie/Data/BookReviewContext.cs
@@ -28,6 +28,131 @@ namespace BookReviewWebAppExercsie.Data
             modelBuilder.Entity<Book>()
                 .HasMany(c => c.Reviews)
                 .WithOne(e => e.Book);
+
+            base.OnModelCreating(modelBuilder);
+
+            Author author1 = new Author
+            {
+                AuthorId = 1,
+                Name = "J.R.R. Tolkien"
+            };
+            Author author2 = new Author
+            {
+                AuthorId = 2,
+                Name = "J.K. Rowling"
+            };
+            Author author3 = new Author
+            {
+                AuthorId = 3,
+                Name = "Stieg Larsson"
+            };
+            Author author4 = new Author
+            {
+                AuthorId = 4,
+                Name = "Gillian Flynn"
+            };
+            Author author5 = new Author
+            {
+                AuthorId = 5,
+                Name = "Matt Haig"
+            };
+            modelBuilder.Entity<Author>().HasData(author1);
+            modelBuilder.Entity<Author>().HasData(author2);
+            modelBuilder.Entity<Author>().HasData(author3);
+            modelBuilder.Entity<Author>().HasData(author4);
+            modelBuilder.Entity<Author>().HasData(author5);
+
+            Book book1 = new Book
+            {
+                BookId = 1,
+                Title = "The Hobbit, or There and Back Again",
+                Genre = "Classics",
+                AuthorId = author1.AuthorId,
+                //Author = author1,
+                Date = new DateTime(2002, 08, 15),
+            };
+            Book book2 = new Book
+            {
+                BookId = 2,
+                Title = "Harry Potter and the Sorcerer's Stone",
+                Genre = "Fantasy",
+                AuthorId = author2.AuthorId,
+                //Author = author2,
+                Date = new DateTime(2003, 11, 01),
+            };
+            Book book3 = new Book
+            {
+                BookId = 3,
+                Title = "The Girl with the Dragon Tattoo",
+                Genre = "Thriller",
+                AuthorId = author3.AuthorId,
+                //Author = author3,
+                Date = new DateTime(2008, 09, 16),
+            };
+            Book book4 = new Book
+            {
+                BookId = 4,
+                Title = "Gone girl",
+                Genre = "Mystery",
+                AuthorId = author4.AuthorId,
+                //Author = author4,
+                Date = new DateTime(2014, 04, 22),
+            };
+            Book book5 = new Book
+            {
+                BookId = 5,
+                Title = "The Midnight Library",
+                Genre = "Fantasy",
+                AuthorId = author5.AuthorId,
+                //Author = author5,
+                Date = new DateTime(2020, 09, 29),
+            };
+            modelBuilder.Entity<Book>().HasData(book1);
+            modelBuilder.Entity<Book>().HasData(book2);
+            modelBuilder.Entity<Book>().HasData(book3);
+            modelBuilder.Entity<Book>().HasData(book4);
+            modelBuilder.Entity<Book>().HasData(book5);
+
+            Review review1 = new Review
+            {
+                ReviewId = 1,
+                Name = "Inge",
+                TextContent = "Cool Story Bro",
+                BookId = book4.BookId
+            };
+            Review review2 = new Review
+            {
+                ReviewId = 2,
+                Name = "Zoe",
+                TextContent = "I will never ever rate this lower than 5 stars. BEAUTIFUL.",
+                BookId = book4.BookId
+            };
+            Review review3 = new Review
+            {
+                ReviewId = 3,
+                Name = "Diane Wallace",
+                TextContent = "Loved it! an interesting,mystery filled and very thrill-seeking storyline..was well written,nice movie also... (paperback!)",
+                BookId = book4.BookId
+            };
+            Review review4 = new Review
+            {
+                ReviewId = 4,
+                Name = "NReads",
+                TextContent = "yo Amy really did THAT",
+                BookId = book4.BookId
+            };
+            Review review5 = new Review
+            {
+                ReviewId = 5,
+                Name = "Emily B",
+                TextContent = "This was cute and the concept was great but unfortunately it really lacked some depth for me!",
+                BookId = book4.BookId
+            };
+            modelBuilder.Entity<Review>().HasData(review1);
+            modelBuilder.Entity<Review>().HasData(review2);
+            modelBuilder.Entity<Review>().HasData(review3);
+            modelBuilder.Entity<Review>().HasData(review4);
+            modelBuilder.Entity<Review>().HasData(review5);
         }
     }
 }
---
+++ BookReviewWebAppExercsie/Migrations/20210610120015_AddExampleData.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookReviewWebAppExercsie.Migrations
+{
+    public partial class AddExampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "AuthorId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "J.R.R. Tolkien" },
+                    { 2, "J.K. Rowling" },
+                    { 3, "Stieg Larsson" },
+                    { 4, "Gillian Flynn" },
+                    { 5, "Matt Haig" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "BookId", "AuthorId", "Date", "Genre", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2002, 8, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Classics", "The Hobbit, or There and Back Again" },
+                    { 2, 2, new DateTime(2003, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fantasy", "Harry Potter and the Sorcerer's Stone" },
+                    { 3, 3, new DateTime(2008, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Thriller", "The Girl with the Dragon Tattoo" },
+                    { 4, 4, new DateTime(2014, 4, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mystery", "Gone girl" },
+                    { 5, 5, new DateTime(2020, 9, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fantasy", "The Midnight Library" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Review",
+                columns: new[] { "ReviewId", "BookId", "Name", "TextContent" },
+                values: new object[,]
+                {
+                    { 1, 4, "Inge", "Cool Story Bro" },
+                    { 2, 4, "Zoe", "I will never ever rate this lower than 5 stars. BEAUTIFUL." },
+                    { 3, 4, "Diane Wallace", "Loved it! an interesting,mystery filled and very thrill-seeking storyline..was well written,nice movie also... (paperback!)" },
+                    { 4, 4, "NReads", "yo Amy really did THAT" },
+                    { 5, 4, "Emily B", "This was cute and the concept was great but unfortunately it really lacked some depth for me!" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "BookId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Review",
+                keyColumn: "ReviewId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Review",
+                keyColumn: "ReviewId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Review",
+                keyColumn: "ReviewId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Review",
+                keyColumn: "ReviewId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Review",
+                keyColumn: "ReviewId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Book",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Author",
+                keyColumn: "AuthorId",
+                keyValue: 4);
+        }
+    }
+}
