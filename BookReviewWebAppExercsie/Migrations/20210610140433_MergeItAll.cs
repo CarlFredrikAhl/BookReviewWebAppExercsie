467d75f7c0def282816cbb84557fa623dd7f1486
LudvigSkold <ludvig.skold@gmail.com>
2021-06-10 16:05:44 +0200

    Rating migration

---
+++ BookReviewWebAppExercsie/Migrations/20210610140433_MergeItAll.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookReviewWebAppExercsie.Migrations
+{
+    public partial class MergeItAll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Book");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Review",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Review");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Book",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
